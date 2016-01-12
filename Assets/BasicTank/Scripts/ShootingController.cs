8e1b8cb8114d5b26f1d3ce3b538c74d4e96cd445
Nacho <mirallei@uji.es>
2016-01-12 16:04:12 +0100

    Some initial Work

---
+++ Assets/BasicTank/Scripts/ShootingController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingController : MonoBehaviour {
+
+    public Transform proyectil;
+    public Transform posicionInicialBala;
+    public int velocidadDisparo = 2;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Transform bala = (Transform)Instantiate(proyectil, posicionInicialBala.position, transform.rotation);
+            bala.GetComponent<Rigidbody>().AddForce(this.transform.forward * 1000 * velocidadDisparo);
+            Destroy(bala.gameObject, 5);
+        }
+	}
+}
