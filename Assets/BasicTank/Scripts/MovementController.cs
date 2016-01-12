a5bd4238e4fcd423055712c46d9caa0f1e1b132c
Nacho <mirallei@uji.es>
2016-01-12 16:35:30 +0100

    a

--- Assets/BasicTank/Scripts/MovementController.cs
+++ Assets/BasicTank/Scripts/MovementController.cs
@@ -2,32 +2,29 @@
 using System.Collections;
 
 public class MovementController : MonoBehaviour
-{//Variables
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-    private Vector3 moveDirection = Vector3.zero;
+{
+    private CharacterController controller;
+    public float velocidadGiro = 2.0f;
+    public float velocidad = 5.0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
-        // is the controller on the ground?
-        if (controller.isGrounded)
-        {
-            //Feed moveDirection with input.
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            //Multiply it by speed.
-            moveDirection *= speed;
-            //Jumping
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
-
-        }
-        //Applying gravity to the controller
-        moveDirection.y -= gravity * Time.deltaTime;
-        //Making the character move
-        controller.Move(moveDirection * Time.deltaTime);
+        Mover();
     }
 
+    void Mover()
+    {
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * velocidadGiro, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float velocidadActual = velocidad * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * velocidadActual);
+
+    }
+   
 }
